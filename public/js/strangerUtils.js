91b819dabaf3b24d3c17c06caa949caf73a93f90
Umed Jadhav <umedj.formal@gmail.com>
2022-06-28 18:36:09 +0530

    Implemented random call connection with strangers

---
+++ public/js/strangerUtils.js
@@ -0,0 +1,21 @@
+import * as wss from './wss.js';
+import * as webRTCHandler from './webRTCHandler.js';
+
+let strangerCallType;
+
+export const changeStrangerConnectionStatus = (status) => {
+    const data = { status};
+    wss.changeStrangerConnectionStatus(data)
+}
+
+export const getStrangerSocketIdAndConnect = (callType) => {
+    strangerCallType = callType;
+    wss.getStrangerSocketId();
+}
+
+export const connectWithStranger = (data) => {
+    console.log('connectWithStranger', data.randomStrangerSocketId);
+    if(data.randomStrangerSocketId){
+        webRTCHandler.sendPreOffer(strangerCallType, data.randomStrangerSocketId);
+    } 
+}
